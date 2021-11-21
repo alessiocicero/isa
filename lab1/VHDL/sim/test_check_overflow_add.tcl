b2b29d4a517e410c7459ea3ebec5c3538c09e7da
alessiocicero <alessio.cicero@studenti.polito.it>
2021-11-21 22:52:53 +0100

    Deleted last useless files

--- lab1/VHDL/sim/test_check_overflow_add.tcl
+++
@@ -1,9 +0,0 @@
-quit -sim
-vcom -93 -work ./work ../src/check_overflow_add.vhd
-vcom -93 -work ./work ../src/tb_check_overflow_add.vhd
-
-vsim work.tb_check_overflow_add
-add wave *
-run 90 ns 
-
-
--- lab1/VHDL/sim/test_check_overflow_sub.tcl
+++
@@ -1,9 +0,0 @@
-quit -sim
-vcom -93 -work ./work ../src/check_overflow_sub.vhd
-vcom -93 -work ./work ../src/tb_check_overflow_sub.vhd
-
-vsim work.tb_check_overflow_sub
-add wave *
-run 90 ns 
-
-
